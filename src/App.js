b3e72f7e8621f8501b16d59e5f1ba4b35958cda6
jeromeski <jeromegee@gmail.com>
2021-05-13 15:26:58 +0800

    styles created

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Card = () => <div className='card'></div>;
+
+const App = () => {
+	return (
+		<div className='page'>
+			<div className='container'>
+				<div className='row'>
+					<div className='col-half'>
+						<Card />
+					</div>
+					<div className='col-half'>
+						<Card />
+					</div>
+					<div className='col-half'>
+						<Card />
+					</div>
+					<div className='col-half'>
+						<Card />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
